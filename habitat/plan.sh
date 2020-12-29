658d89640a5a5c2beb30f94a23115ab1c82e50e3
Riju <rijumavilayi@gmail.com>
2020-12-29 07:10:28 +0530

    updated version

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -4,7 +4,7 @@ pkg_origin=blackhole
 
 # Required.
 # Sets the version of the package
-pkg_version="0.1.3"
+pkg_version="0.1.4"
 
 # Optional.
 # The name and email address of the package maintainer.
